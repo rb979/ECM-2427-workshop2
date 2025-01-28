ea174d69d9e07e210f63c1283bd994e254d085ff
ICannotCodeGG <gzyk201@exeter.ac.uk>
2025-01-29 02:05:46 +0800

    Update urls.py following the API

--- djangoting/mysite/urls.py
+++ djangoting/mysite/urls.py
@@ -18,9 +18,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_shopping_list/', views.create_shopping_list, name='create_shopping_list'),
-    path('add_item_to_list/<int:list_id>/', views.add_item_to_list, name='add_item_to_list'),
-    path('remove_item_from_list/<int:list_id>/<int:item_id>/', views.remove_item_from_list, name='remove_item_from_list'),
-    path('list_items/<int:list_id>/', views.list_items, name='list_items'),
+    path('api/v1/addItem', views.add_item, name='add_item'),
+    path('api/v1/removeItem', views.remove_item, name='remove_item'),
+    path('api/v1/removeItems', views.remove_items, name='remove_items'),
+    path('api/v1/newType', views.new_type, name='new_type'),
+    path('api/v1/removeType', views.remove_type, name='remove_type'),
+    path('api/v1/addToShoppingList', views.add_to_shopping_list, name='add_to_shopping_list'),
+    path('api/v1/removeFromShoppingList', views.remove_from_shopping_list, name='remove_from_shopping_list'),
+    path('api/v1/purchaseItem', views.purchase_item, name='purchase_item'),
 ]
 
